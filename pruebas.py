a67b5c1fd8182c88050d1ab3251fc6d78d77f84b
Benitez4721 <benitez4721@gmail.com>
2020-01-30 17:36:17 -0400

    Segundo caso de prueba

--- pruebas.py
+++ pruebas.py
@@ -1,11 +1,16 @@
 import unittest
-from Pension import Persona
+from pension import Persona
 
 
 class pensionTest(unittest.TestCase):
+    
     def testVaron60anos750cotizaciones(self):
         self.persona = Persona("1/1/1960",'M',750,0)
         self.assertTrue(self.persona._EsPensionado())
+
+    def testVaron59anos750cotizacionesSinAnosInsalubres(self):
+        self.persona = Persona("1/1/1961",'M',750,0)
+        self.assertFalse(self.persona._EsPensionado())        
 
 
 if __name__ == '__main__':
